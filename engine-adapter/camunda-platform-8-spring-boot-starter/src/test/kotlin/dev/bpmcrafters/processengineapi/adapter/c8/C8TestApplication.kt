35d38ce5c22979ae0619eae87311d747735cbf1a
Simon Zambrovski <simon.zambrovski@holisticon.de>
2025-03-31 12:08:18 +0200

    add content

---
+++ engine-adapter/camunda-platform-8-spring-boot-starter/src/test/kotlin/dev/bpmcrafters/processengineapi/adapter/c8/C8TestApplication.kt
@@ -0,0 +1,9 @@
+package dev.bpmcrafters.processengineapi.adapter.c8
+
+import com.tngtech.jgiven.integration.spring.EnableJGiven
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+
+@EnableJGiven
+@SpringBootApplication
+class C8TestApplication
