ff9cadb0b708001c55f02bd3f1abaa76df8e5069
Jan Rohwer <jan.rohwer@holisticon.de>
2026-02-20 12:41:17 +0100

    downgrade springdoc-openapi due to spring boot 3 and add kotlin jackson support

--- examples/java-c8/src/main/java/dev/bpmcrafters/example/javac8/JavaCamunda8ExampleApplication.java
+++ examples/java-c8/src/main/java/dev/bpmcrafters/example/javac8/JavaCamunda8ExampleApplication.java
@@ -3,7 +3,6 @@ package dev.bpmcrafters.example.javac8;
 import dev.bpmcrafters.processengineapi.adapter.c8.springboot.C8AdapterProperties;
 import io.camunda.client.spring.properties.CamundaClientProperties;
 import io.camunda.tasklist.CamundaTaskListClient;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
